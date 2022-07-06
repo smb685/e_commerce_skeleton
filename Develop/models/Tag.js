45d3ba7dce97476b69dd182ee5963ec6d2befa23
smb685 <sylvester.brandon@shipt.com>
2022-07-06 11:26:23 -0500

    defined the colums

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrament: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
